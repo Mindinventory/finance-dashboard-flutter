471c1fbca9fd530e6dee52938b80f388d42012a9
Mohit Chauhan <mohit.chauhan@mindinventory.com>
2021-08-26 13:06:00 +0000

    [feature] added support for responsive view

--- lib/common/app_routes.dart
+++ lib/common/app_routes.dart
@@ -1,26 +1,15 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
-import '../ui/home_module/home_page.dart';
-import '../ui/login/login_page.dart';
-import '../ui/spalsh_module/spalsh_page.dart';
+import '../ui/home_module/dashboard.dart';
 
 final routes = [
   GetPage(
-    name: Routes.root,
-    page: () => SplashPage(),
-  ),
-  GetPage(
-    name: Routes.login,
-    page: () => LoginPage(),
-  ),
-  GetPage(
-    name: Routes.home,
-    page: () => const HomePage(),
+    name: Routes.dashboard,
+    page: () => const DashBoard(),
   ),
 ];
 
 abstract class Routes {
   static const root = '/';
-  static const login = '/login';
-  static const home = '/home';
+  static const dashboard = '/dashboard';
 }
